784f9b530c2b157dcc1cce29175b9e08ad0bd2bb
Divyapriyadharshini Babu <divyapriyadharshinibabu@gmail.com>
2022-06-23 17:38:51 -0700

    Setup basic start page and display single question

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+var questions = [
+  {
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  }
+];
+
+//selecting all required elements
+const start_btn = document.querySelector(".start_btn");
+
+// if startQuiz button clicked
+start_btn.addEventListener("click", function() {
+    // startTimer()
+    // displayCurrentQuestion()
+    displayCurrentQuestion(0);
+});
+
+function displayCurrentQuestion(index) {
+    // Get div infobox
+    var divInfobox = document.querySelector(".info_box");
+
+    // Create a div element for que box  - <div class="que-text">Question</div>
+    var divTag = document.createElement("div");
+    divTag.setAttribute("class", "que-box");
+
+    // Create a div element for que
+    var divQuetext = document.createElement("div");
+    divQuetext.setAttribute("class", "que-text");
+    divQuetext.textContent = questions[index]['question'];
+
+    // Create div element for options - <div class=>
+    var divQueoptions = document.createElement("div");
+    divQueoptions.setAttribute("class", "que-options");
+
+    // Create button  <button class="start_btn">Start Quiz</button>
+    for (var i = 0; i < 4; i++) {
+
+        var optionButton = document.createElement("button");
+        optionButton.setAttribute("class", "option");
+        optionButton.textContent = questions[index]['options'][i];
+
+        divQueoptions.appendChild(optionButton);
+
+        var newLine = document.createElement("br");
+        divQueoptions.appendChild(newLine);
+    }
+
+    // Append que and options to que box
+    divTag.appendChild(divQuetext);
+    divTag.appendChild(divQueoptions);
+        
+    // Set inner html of info box to que box
+    divInfobox.parentNode.replaceChild(divTag, divInfobox);
+}
